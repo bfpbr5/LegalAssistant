06660e71086122751b0d30d28264325e6e6371c8
bfpbr5 <bffpbr5@gmail.com>
2023-07-16 14:22:36 +0800

    init ai function

---
+++ ai_unit.py
@@ -0,0 +1,75 @@
+import openai
+import promptlayer
+import streamlit as st
+
+promptlayer.api_key = "pl_6693b063dd1e5bc294f4fb3e18820039"
+openai = promptlayer.openai
+openai.api_key = st.secrets["openai"]["api_key"]
+
+class CaseAnalyzer:
+    def __init__(self):
+        # Initialize the OpenAI API
+        pass
+
+    def analyze(self, case_text):
+        # Send the case details to the API and get the response
+        response = openai.ChatCompletion.create(
+          model="gpt-3.5-turbo",
+          messages=[
+                {"role": "system", "content": """Suppose you're a seasoned Chinese attorney well-versed in the civil law system. A user will share a case with you for review. Structure your response following below: 
+1) Analyzing the case to discern the involved legal relations,
+2) Establishing the basis for the claim,
+3) Identifying the litigation request presented in the case, and 
+4) Evaluating if the given information is comprehensive and seeking further clarification if necessary."""},
+                {"role": "user", "content": case_text}
+            ]
+        )
+        # Return the assistant's reply
+        return response['choices'][0]['message']['content']
+
+
+class EvidenceAnalyzer:
+    def __init__(self):
+        # Initialize the OpenAI API
+        pass
+
+    def analyze(self, evidence_file):
+        # Since GPT-3 doesn't support image analysis, we'll return a placeholder
+        return "GPT-3 does not support image or document analysis."
+
+
+class SimilarCaseFinder:
+    def __init__(self):
+        # Initialize the OpenAI API
+        pass
+
+    def find(self, keywords, geog_pref):
+        # Send the keywords and geographical preference to the API and get the response
+        query = f"Find similar cases related to {keywords} in {geog_pref}."
+        response = openai.ChatCompletion.create(
+          model="gpt-3.5-turbo",
+          messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": query}
+            ]
+        )
+        # Return the assistant's reply
+        return response['choices'][0]['message']['content']
+
+
+class LitigationStrategist:
+    def __init__(self):
+        # Initialize the OpenAI API
+        pass
+
+    def generate(self, style):
+        # Send the litigation style to the API and get the response
+        response = openai.ChatCompletion.create(
+          model="gpt-3.5-turbo",
+          messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": f"Generate a {style} litigation strategy."}
+            ]
+        )
+        # Return the assistant's reply
+        return response['choices'][0]['message']['content']
---
+++ app.py
@@ -0,0 +1,45 @@
+import streamlit as st
+# import openai
+# from pinecone import PineconeClient
+from ai_unit import CaseAnalyzer, EvidenceAnalyzer, SimilarCaseFinder, LitigationStrategist
+
+case_analyzer = CaseAnalyzer()
+# evidence_analyzer = EvidenceAnalyzer()
+# similar_case_finder = SimilarCaseFinder()
+# litigation_strategist = LitigationStrategist()
+
+st.sidebar.title("AI Legal Assistant")
+function = st.sidebar.selectbox("Select a function", ["Case analysis", "Evidence analysis", "Similar case analysis", "Litigation strategy"])
+
+if function == "Case analysis":
+    st.title("Case Analysis")
+    case_text = st.text_area("Enter the details of your case:")
+    if st.button("Analyze Case"):
+        # result = case_analyzer.analyze(case_text)
+        result = case_analyzer.analyze(case_text)
+        st.write(result)
+
+elif function == "Evidence analysis":
+    st.title("Evidence Analysis")
+    evidence_file = st.file_uploader("Upload your evidence:")
+    if st.button("Analyze Evidence"):
+        # result = evidence_analyzer.analyze(evidence_file)
+        result = "Analyzed Evidence"
+        st.write(result)
+
+elif function == "Similar case analysis":
+    st.title("Similar Case Analysis")
+    keywords = st.text_input("Enter keywords for retrieval:")
+    geog_pref = st.text_input("Enter geographical preference:")
+    if st.button("Find Similar Cases"):
+        # result = similar_case_finder.find(keywords, geog_pref)
+        result = "Found Similar Cases"
+        st.write(result)
+
+elif function == "Litigation strategy":
+    st.title("Litigation Strategy")
+    style = st.selectbox("Select a litigation style", ["Aggressive", "Defensive", "Balanced"])
+    if st.button("Generate Strategy"):
+        # result = litigation_strategist.generate(style)
+        result = "Generated Strategy"
+        st.write(result)
