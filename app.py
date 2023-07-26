c2792b37120dce7856a1409874ed75a5fcd4a5a8
bfpbr5 <bffpbr5@gmail.com>
2023-07-27 02:00:17 +0800

    add next button & uploads

--- app.py
+++ app.py
@@ -14,7 +14,6 @@ if function == "Case analysis":
     case_text = st.text_area("Enter the details of your case:")
     if st.button("Analyze Case"):
         # result = case_analyzer.analyze(case_text)
-        # result = case_analyzer.analyze(case_text)
         result = "1. 在这个案件中……\n\n2. 请求权基础主要是……\n\n3. 在这个案件中，原告的诉讼请求可能包括……\n\n4. 尽管提供的信息相对完整，但仍需要进一步澄清一些问题……"
         result_parts = result.split("\n\n")
         result_parts = [part.split('. ', 1)[-1] for part in result_parts]
@@ -22,14 +21,32 @@ if function == "Case analysis":
         for i in range(4):
             st.subheader(part_names[i])
             st.write(result_parts[i])
+        if st.button("Next: Evidence Analysis"):
+            function = "Evidence analysis"
 
 elif function == "Evidence analysis":
     st.title("Evidence Analysis")
-    evidence_file = st.file_uploader("Upload your evidence:")
-    if st.button("Analyze Evidence"):
-        # result = evidence_analyzer.analyze(evidence_file)
-        result = "Analyzed Evidence"
-        st.write(result)
+    evidence_input_method = st.radio("How would you like to provide your evidence?", ["Upload Image", "Upload PDF", "Enter Text"])
+    if evidence_input_method == "Upload Image":
+        evidence_image = st.file_uploader("Upload your evidence image:", type=["png", "jpg", "jpeg"])
+        if st.button("Analyze Image Evidence"):
+            # result = evidence_analyzer.analyze_image(evidence_image)
+            result = "Analyzed Image Evidence"
+            st.write(result)
+    elif evidence_input_method == "Upload PDF":
+        evidence_pdf = st.file_uploader("Upload your evidence PDF:", type=["pdf"])
+        if st.button("Analyze PDF Evidence"):
+            # result = evidence_analyzer.analyze_pdf(evidence_pdf)
+            result = "Analyzed PDF Evidence"
+            st.write(result)
+    else:
+        evidence_text = st.text_area("Enter your evidence text:")
+        if st.button("Analyze Text Evidence"):
+            # result = evidence_analyzer.analyze_text(evidence_text)
+            result = "Analyzed Text Evidence"
+            st.write(result)
+    if st.button("Next: Similar Case Analysis"):
+        function = "Similar case analysis"
 
 elif function == "Similar case analysis":
     st.title("Similar Case Analysis")
@@ -39,6 +56,8 @@ elif function == "Similar case analysis":
         # result = similar_case_finder.find(keywords, geog_pref)
         result = "Found Similar Cases"
         st.write(result)
+        if st.button("Next: Litigation Strategy"):
+            function = "Litigation strategy"
 
 elif function == "Litigation strategy":
     st.title("Litigation Strategy")
@@ -47,3 +66,5 @@ elif function == "Litigation strategy":
         # result = litigation_strategist.generate(style)
         result = "Generated Strategy"
         st.write(result)
+        if st.button("Start Over"):
+            function = "Case analysis"
