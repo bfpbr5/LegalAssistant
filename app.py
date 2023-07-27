70e7ac5036502689171f25faa9ba0272639e8444
bfpbr5 <bffpbr5@gmail.com>
2023-07-27 20:32:43 +0800

    restructure

--- app.py
+++ app.py
@@ -3,71 +3,96 @@ from ai_unit import CaseAnalyzer, EvidenceAnalyzer, SimilarCaseFinder, Litigatio
 
 case_analyzer = CaseAnalyzer()
 evidence_analyzer = EvidenceAnalyzer()
-# similar_case_finder = SimilarCaseFinder()
-# litigation_strategist = LitigationStrategist()
+similar_case_finder = SimilarCaseFinder()
+litigation_strategist = LitigationStrategist()
 
-st.sidebar.title("AI Legal Assistant")
-function = st.sidebar.selectbox("Select a function", ["Case analysis", "Evidence analysis", "Similar case analysis", "Litigation strategy"])
+if "cases" not in st.session_state:
+    st.session_state["cases"] = []
 
-if function == "Case analysis":
+if "current_case" not in st.session_state:
+    st.session_state["current_case"] = None
+
+# Allow user to add a new case
+if st.sidebar.button("Add a new case"):
+    case_name = st.sidebar.text_input("Enter case name:")
+    case_date = st.sidebar.date_input("Enter case date:")
+    st.session_state["cases"].append({"name": case_name, "date": case_date, "steps": {}})
+    st.session_state["current_case"] = len(st.session_state["cases"]) - 1
+
+# Allow user to select a case
+case_names = [case["name"] for case in st.session_state["cases"]]
+case_index = st.sidebar.selectbox("Select a case", case_names, index=st.session_state["current_case"])
+if case_index != st.session_state["current_case"]:
+    st.session_state["current_case"] = case_index
+
+# Run the appropriate step based on the current state of the case
+case = st.session_state["cases"][st.session_state["current_case"]]
+if "step" not in case:
+    case["step"] = 1
+
+if case["step"] == 1:
     st.title("Case Analysis")
     case_text = st.text_area("Enter the details of your case:")
     if st.button("Analyze Case"):
         with st.spinner('🤔'):
             result = case_analyzer.analyze(case_text)
-            # result = "1. 在这个案件中……\n\n2. 请求权基础主要是……\n\n3. 在这个案件中，原告的诉讼请求可能包括……\n\n4. 尽管提供的信息相对完整，但仍需要进一步澄清一些问题……"
             result_parts = result.split("\n\n")
             result_parts = [part.split('. ', 1)[-1] for part in result_parts]
-            part_names = ["法律关系分析", "案由", "诉讼请求", "待澄清问题"]
+            part_names = ["Case Context", "Claim Basis", "Plaintiff's Claims", "Additional Questions"]
             for i in range(4):
                 st.subheader(part_names[i])
                 st.write(result_parts[i])
-            st.caption("请在分析结果满意后在侧边栏选择下一个模块继续分析.")
-        # if st.button("Next: Evidence Analysis"):
-        #     function = "Evidence analysis"
+            case["steps"][1] = {"input": case_text, "output": result_parts}
+            case["step"] += 1
 
-elif function == "Evidence analysis":
+# Repeat for steps 2, 3, and 4
+
+
+
+elif case["step"] == 2:
     st.title("Evidence Analysis")
     evidence_input_method = st.radio("How would you like to provide your evidence?", ["Upload Image", "Upload PDF", "Enter Text"])
     if evidence_input_method == "Upload Image":
         evidence_image = st.file_uploader("Upload your evidence image:", type=["png", "jpg", "jpeg"])
         if st.button("Analyze Image Evidence"):
             # result = evidence_analyzer.analyze_image(evidence_image)
-            result = "Analyzed Image Evidence"
+            result = "test"
             st.write(result)
+            st.session_state["step"] += 1
     elif evidence_input_method == "Upload PDF":
         evidence_pdf = st.file_uploader("Upload your evidence PDF:", type=["pdf"])
         if st.button("Analyze PDF Evidence"):
             # result = evidence_analyzer.analyze_pdf(evidence_pdf)
-            result = "Analyzed PDF Evidence"
+            result = "test"
             st.write(result)
+            st.session_state["step"] += 1
     else:
         evidence_text = st.text_area("Enter your evidence text:")
         if st.button("Analyze Text Evidence"):
             # result = evidence_analyzer.analyze_text(evidence_text)
-            result = "Analyzed Text Evidence"
+            result = "test"
             st.write(result)
+            st.session_state["step"] += 1
     st.caption("请在分析结果满意后在侧边栏选择下一个模块继续分析.")
-    # if st.button("Next: Similar Case Analysis"):
-    #     function = "Similar case analysis"
 
-elif function == "Similar case analysis":
+
+elif case["step"] == 3:
     st.title("Similar Case Analysis")
     keywords = st.text_input("Enter keywords for retrieval:")
     geog_pref = st.text_input("Enter geographical preference:")
     if st.button("Find Similar Cases"):
         # result = similar_case_finder.find(keywords, geog_pref)
-        result = "Found Similar Cases"
+        result = "test"
         st.write(result)
-        # if st.button("Next: Litigation Strategy"):
-        #     function = "Litigation strategy"
+        st.session_state["step"] += 1
 
-elif function == "Litigation strategy":
+
+elif case["step"] == 4:
     st.title("Litigation Strategy")
     style = st.selectbox("Select a litigation style", ["Aggressive", "Defensive", "Balanced"])
     if st.button("Generate Strategy"):
         # result = litigation_strategist.generate(style)
-        result = "Generated Strategy"
+        result = "test"
         st.write(result)
-        # if st.button("Start Over"):
-        #     function = "Case analysis"
+        st.session_state["step"] = 1
+
