63c0e77206d21ae385d4c3b813c3c6a955c9adfa
bfpbr5 <bffpbr5@gmail.com>
2023-07-27 14:35:11 +0800

    disable next buttons and enable AI

--- app.py
+++ app.py
@@ -13,8 +13,8 @@ if function == "Case analysis":
     st.title("Case Analysis")
     case_text = st.text_area("Enter the details of your case:")
     if st.button("Analyze Case"):
-        # result = case_analyzer.analyze(case_text)
-        result = "1. 在这个案件中……\n\n2. 请求权基础主要是……\n\n3. 在这个案件中，原告的诉讼请求可能包括……\n\n4. 尽管提供的信息相对完整，但仍需要进一步澄清一些问题……"
+        result = case_analyzer.analyze(case_text)
+        # result = "1. 在这个案件中……\n\n2. 请求权基础主要是……\n\n3. 在这个案件中，原告的诉讼请求可能包括……\n\n4. 尽管提供的信息相对完整，但仍需要进一步澄清一些问题……"
         result_parts = result.split("\n\n")
         result_parts = [part.split('. ', 1)[-1] for part in result_parts]
         part_names = ["法律关系分析", "请求权基础", "诉讼请求", "待澄清问题"]
@@ -22,8 +22,8 @@ if function == "Case analysis":
             st.subheader(part_names[i])
             st.write(result_parts[i])
         st.caption("请在分析结果满意后在侧边栏选择下一个模块继续分析.")
-        if st.button("Next: Evidence Analysis"):
-            function = "Evidence analysis"
+        # if st.button("Next: Evidence Analysis"):
+        #     function = "Evidence analysis"
 
 elif function == "Evidence analysis":
     st.title("Evidence Analysis")
