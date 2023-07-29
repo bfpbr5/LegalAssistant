9f11902b2a331f15c957d838dd8a5d4962ddaa1a
bfpbr5 <bffpbr5@gmail.com>
2023-07-29 15:01:06 +0800

    wrap into class

--- app.py
+++ app.py
@@ -37,8 +37,7 @@ if case["step"] >= 1:
         with st.spinner('🤔'):
             # result = case_analyzer.analyze(case_text)
             result = "1. 在这个案件中……\n\n2. 请求权基础主要是……\n\n3. 在这个案件中，原告的诉讼请求可能包括……\n\n4. 尽管提供的信息相对完整，但仍需要进一步澄清一些问题……"
-            result_parts = result.split("\n\n")
-            result_parts = [part.split('. ', 1)[-1] for part in result_parts]
+            result_parts = case_analyzer.split_analysis(result)
             part_names = ["Case Context", "Claim Basis", "Plaintiff's Claims", "Additional Questions"]
             for i in range(4):
                 st.subheader(part_names[i])
