8aeb0fa4f4b11b888ee4d42678eef0ff78676ff5
bfpbr5 <bffpbr5@gmail.com>
2023-07-29 14:23:34 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,27 +7,26 @@ similar_case_finder = SimilarCaseFinder()
 litigation_strategist = LitigationStrategist()
 
 # Initialize session state
-st.session_state.setdefault("cases", [{"name": "Initial Case", "date": st.date_input('Please select a date', value=None), "steps": {}}])
+st.session_state.setdefault("cases", [{"name": "Initial Case", "date": None, "steps": {}}])
 st.session_state.setdefault("current_case", 0)
 
-def add_new_case():
-    case_name = st.sidebar.text_input("Enter case name:")
-    case_date = st.sidebar.date_input("Enter case date:")
-    st.session_state["cases"].append({"name": case_name, "date": case_date, "steps": {}})
-    st.session_state["current_case"] = int(len(st.session_state["cases"]) - 1)
+with st.sidebar.form("Add case form"):
+    case_name = st.text_input("Enter case name:")
+    case_date = st.date_input("Enter case date:")
+    submitted = st.form_submit_button("Add a new case")
 
-# Allow user to add a new case
-if st.sidebar.button("Add a new case"):
-    add_new_case()
+if submitted:
+    st.session_state["cases"].append({"name": case_name, "date": case_date, "steps": {}})
+    st.session_state["current_case"] = len(st.session_state["cases"]) - 1
 
 # Allow user to select a case
 case_names = [case["name"] for case in st.session_state["cases"]] if st.session_state["cases"] else ["No cases"]
 case_index = st.sidebar.selectbox("Select a case", case_names, index=int(st.session_state["current_case"]) if st.session_state["current_case"] is not None else 0)
-if case_index != st.session_state["current_case"]:
+if case_names[case_index] != st.session_state["cases"][st.session_state["current_case"]]["name"]:
     st.session_state["current_case"] = case_names.index(case_index)
 
 # Run the appropriate step based on the current state of the case
-case = st.session_state["cases"][int(st.session_state["current_case"])]
+case = st.session_state["cases"][st.session_state["current_case"]]
 case.setdefault("step", 1)
 
 # Rest of your code
